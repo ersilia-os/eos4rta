d6d71f969d0b5559d0fcc263eebffc0eb3ca804e
gemmaturon <gemma@ersilia.io>
2025-11-24 19:08:16 +0100

    update model to lq

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,8 +1,7 @@
-# imports
 import os
 import csv
 import sys
-import joblib
+from lazyqsar.qsar import LazyBinaryQSAR
 
 # parse arguments
 input_file = sys.argv[1]
@@ -13,18 +12,7 @@ root = os.path.dirname(os.path.abspath(__file__))
 
 MODELPATH = os.path.join(root, "..", "..", "checkpoints")
 
-# my model
-def my_model(smiles_list):
-    mdl1 = joblib.load(os.path.join(MODELPATH, "ic50_nf54_ldh_72h_1uM_morgan.joblib"))
-    mdl2 = joblib.load(os.path.join(MODELPATH, "perc_nf54_ldh_72h_50_morgan.joblib"))
-    mdl3 = joblib.load(os.path.join(MODELPATH, "perc_nf54_lum_72h_50_unders_morgan.joblib"))
-
-    y_pred1 = mdl1.predict_proba(smiles_list)[:,1]
-    y_pred2 = mdl2.predict_proba(smiles_list)[:,1]
-    y_pred3 = mdl3.predict_proba(smiles_list)[:,1]
-
-    return y_pred1, y_pred2, y_pred3
-
+models = ["ic50_nf54_ldh_72h_1uM", "ic50_nf54_lum_72h_1uM", "perc_nf54_ldh_72h_50", "perc_nf54_lum_72h_50"]
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -33,11 +21,18 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
 
 # run model
-output1, output2, output3 = my_model(smiles_list)
+y_preds = {}
+for m in models:
+    model_folder = os.path.join(MODELPATH, f"{m}")
+    model = LazyBinaryQSAR.load(f"{model_folder}")
+    y_pred = model.predict_proba(smiles_list=smiles_list)[:, 1]
+    y_preds[m]=y_pred
+
+header = list(y_preds.keys())
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["NF54_IC50_72h_1uM".lower(), "NF54_50PercInh_LDH".lower(), "NF54_50PercInh_LUM".lower()])  # header with column names
-    for o1, o2, o3 in zip(output1, output2, output3):
-        writer.writerow([o1, o2, o3])
+    writer.writerow(["ic50_nf54_ldh_72h_1uM", "ic50_nf54_lum_72h_1uM", "perc_nf54_ldh_72h_50", "perc_nf54_lum_72h_50"]) 
+    for o1, o2, o3, o4 in zip(*(y_preds[m].tolist() for m in header)):
+        writer.writerow([o1, o2, o3, o4])
